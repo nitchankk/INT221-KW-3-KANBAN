464a034e6108dd66fc0fa750f534c6fa93691c9e
natdnai tessapan <natdanai.tess@gmail.com>
2024-05-28 22:01:40 +0700

    final

---
+++ backend1/src/main/java/com/example/integradeproject/dtos/NewTask2DTO.java
@@ -0,0 +1,46 @@
+package com.example.integradeproject.dtos;
+
+import lombok.*;
+
+//@Getter
+//@Setter
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class NewTask2DTO {
+//    private Integer taskId ;
+//    private String title ;
+//    private String description ;
+//    private String assignees ;
+//    private String  statusName ;
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewTask2DTO {
+    private Integer taskId;
+    private String title;
+    private String description;
+    private String assignees;
+    private String statusName; // Changed the property name from statusName to statusName
+
+    // Getters and setters
+
+public void setTitle(String title){
+    this.title = title;
+}
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public void setAssignees(String assignees){
+        this.assignees = assignees;
+    }
+    public void setStatusName(String statusName){
+        this.statusName = statusName;
+    }
+
+
+
+}
